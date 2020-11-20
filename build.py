a5388e3fe927ceab1fdd64adf39706c5d24ab4ad
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-20 14:01:51 +0100

    Appy new universal build foundation

--- build.py
+++ build.py
@@ -1,131 +1,77 @@
-import argparse
 import os
-import re
-import sys
-from shutil import rmtree
-from typing import Dict, Union
 
 from universal_build import build_utils
+from universal_build.helpers import build_python
 
+# Project specific configuration
 MAIN_PACKAGE = "lazydocs"
 GITHUB_URL = "https://github.com/ml-tooling/lazydocs"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-def main(args: Dict[str, Union[bool, str]]):
-
+def main(args: dict) -> None:
     # set current path as working dir
     os.chdir(HERE)
 
-    version = args[build_utils.FLAG_VERSION]
+    version = args.get(build_utils.FLAG_VERSION)
 
     if version:
-        # Replace version in about.py
-        with open(os.path.join(HERE, f"src/{MAIN_PACKAGE}/_about.py"), "r+") as f:
-            data = f.read()
-            f.seek(0)
-            f.write(re.sub(r"__version__ = \".+\"", f'__version__ = "{version}"', data))
-            f.truncate()
+        # Update version in _about.py
+        build_python.update_version(
+            os.path.join(HERE, f"src/{MAIN_PACKAGE}/_about.py"), str(version)
+        )
 
-    if args[build_utils.FLAG_CHECK]:
-        _check(args)
+    if args.get(build_utils.FLAG_MAKE):
+        # Install pipenv dev requirements
+        build_python.install_build_env()
+        # Create API documentation via lazydocs
+        build_python.generate_api_docs(github_url=GITHUB_URL, main_package=MAIN_PACKAGE)
+        # Build distribution via setuptools
+        build_python.build_distribution()
 
-    if args[build_utils.FLAG_MAKE]:
-        _make(args)
+    if args.get(build_utils.FLAG_CHECK):
+        build_python.code_checks(exit_on_error=True)
 
-    if args[build_utils.FLAG_TEST]:
-        _test(args)
+    if args.get(build_utils.FLAG_TEST):
+        # Remove coverage files
+        build_utils.run("pipenv run coverage erase", exit_on_error=False)
 
-    if args[build_utils.FLAG_RELEASE]:
-        _release(args)
+        test_markers = args.get(build_utils.FLAG_TEST_MARKER)
 
+        if (
+            isinstance(test_markers, list)
+            and build_utils.TEST_MARKER_SLOW in test_markers
+        ):
+            # Run if slow test marker is set: test in multiple environments
+            # Python 3.6
+            build_python.test_with_py_version(python_version="3.6.12")
 
-def _check(args: Dict[str, Union[bool, str]]):
-    """Run linting and style checks via black, isort, mypy and flake8."""
-    build_utils.run("pip install -e .[dev]", exit_on_error=True)
-    # Run linters and checks
-    build_utils.run("black --check src", exit_on_error=True)
-    build_utils.run("black --check tests", exit_on_error=True)
-    build_utils.run("isort --profile black --check-only src", exit_on_error=True)
-    build_utils.run("isort --profile black --check-only tests", exit_on_error=True)
-    build_utils.run("mypy src", exit_on_error=True)
-    build_utils.run("flake8 --show-source --statistics src", exit_on_error=True)
-    build_utils.run("flake8 --show-source --statistics tests", exit_on_error=True)
-    build_utils.run("pydocstyle src", exit_on_error=True)
+            # Python 3.7
+            build_python.test_with_py_version(python_version="3.7.9")
 
+            # Activated Python Environment (3.8)
+            build_python.install_build_env()
+            # Run pytest in pipenv environment
+            build_utils.run("pipenv run pytest", exit_on_error=True)
 
-def _make(args: Dict[str, Union[bool, str]]):
-    """Build the library."""
-    try:
-        # Ensure there are no old builds
-        rmtree(os.path.join(HERE, "dist"))
-    except OSError:
-        pass
+            # Update pipfile.lock when all tests are successfull (lock environment)
+            build_utils.run("pipenv lock", exit_on_error=True)
+        else:
+            # Run fast tests
+            build_utils.run('pipenv run pytest -m "not slow"', exit_on_error=True)
 
-    # Build the distribution archives
-    build_utils.run(
-        f"{sys.executable} setup.py sdist bdist_wheel clean --all",
-        exit_on_error=True,
-    )
+    if args.get(build_utils.FLAG_RELEASE):
+        # Publish distribution on pypi
+        build_python.publish_pypi_distribution(
+            pypi_token=args.get(build_python.FLAG_PYPI_TOKEN),
+            pypi_repository=args.get(build_python.FLAG_PYPI_REPOSITORY),
+        )
 
-    # Check the archives with twine
-    build_utils.run("twine check dist/*", exit_on_error=True)
-
-    # Install library
-    build_utils.run("pip install -e .[dev]", exit_on_error=True)
-    # Create API documentation via lazydocs
-    build_utils.run(
-        f"lazydocs --overview-file=README.md --src-base-url={GITHUB_URL}/blob/main {MAIN_PACKAGE}",
-        exit_on_error=True,
-    )
-
-
-def _test(args: Dict[str, Union[bool, str]]):
-    """Run all tests."""
-    # Install library
-    build_utils.run("pip install -e .[dev]", exit_on_error=True)
-    # Execute tests with coverage check
-    # build_utils.run(
-    #     f"pytest --cov={MAIN_PACKAGE} --cov-report=xml --cov-report term --cov-report=html tests",
-    #     exit_on_error=True,
-    # )
-
-
-def _release(args: Dict[str, Union[bool, str]]):
-    """Publish library to pypi repository."""
-    pypi_user = "__token__"
-    pypi_repository_args = ""  # Use main repository
-
-    if "pypi_token" in args:
-        pypi_token = args["pypi_token"]
-    else:
-        build_utils.log("PyPI token is required for release (--pypi-token=<TOKEN>)")
-        build_utils.exit_process(1)
-
-    if "pypi_repository" in args and args["pypi_repository"]:
-        pypi_repository_args = '--repository-url "' + str(args["pypi_repository"]) + '"'
-
-    # Publish on pypi
-    build_utils.run(
-        f'twine upload --non-interactive -u "{pypi_user}" -p "{pypi_token}" {pypi_repository_args} dist/*',
-        exit_on_error=True,
-    )
-
-    # Publish coverage report: if private repo set CODECOV_TOKEN="token" or use -t
-    build_utils.run("curl -s https://codecov.io/bash | bash -s", exit_on_error=False)
+        # TODO: Publish coverage report: if private repo set CODECOV_TOKEN="token" or use -t
+        # build_utils.run("curl -s https://codecov.io/bash | bash -s", exit_on_error=False)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--pypi-token", help="Personal access token for PyPI account.", required=False
-    )
-    parser.add_argument(
-        "--pypi-repository",
-        help="PyPI repository for publishing artifacts.",
-        required=False,
-    )
-
-    args = build_utils.get_sanitized_arguments(argument_parser=parser)
+    args = build_python.get_sanitized_arguments()
     main(args)
