5e150a20e5cbc4b1e75d7cc6c1549d552df98ef8
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-24 16:27:05 +0100

    Update to universal build version 0.3.2

--- build.py
+++ build.py
@@ -19,16 +19,20 @@ def main(args: dict) -> None:
     if version:
         # Update version in _about.py
         build_python.update_version(
-            os.path.join(HERE, f"src/{MAIN_PACKAGE}/_about.py"), str(version)
+            os.path.join(HERE, f"src/{MAIN_PACKAGE}/_about.py"),
+            str(version),
+            exit_on_error=True,
         )
 
     if args.get(build_utils.FLAG_MAKE):
         # Install pipenv dev requirements
-        build_python.install_build_env()
+        build_python.install_build_env(exit_on_error=True)
         # Create API documentation via lazydocs
-        build_python.generate_api_docs(github_url=GITHUB_URL, main_package=MAIN_PACKAGE)
+        build_python.generate_api_docs(
+            github_url=GITHUB_URL, main_package=MAIN_PACKAGE, exit_on_error=True
+        )
         # Build distribution via setuptools
-        build_python.build_distribution()
+        build_python.build_distribution(exit_on_error=True)
 
     if args.get(build_utils.FLAG_CHECK):
         build_python.code_checks(exit_on_error=True)
@@ -37,21 +41,20 @@ def main(args: dict) -> None:
         # Remove coverage files
         build_utils.run("pipenv run coverage erase", exit_on_error=False)
 
-        test_markers = args.get(build_utils.FLAG_TEST_MARKER)
-
-        if (
-            isinstance(test_markers, list)
-            and build_utils.TEST_MARKER_SLOW in test_markers
-        ):
+        if build_utils.TEST_MARKER_SLOW in args.get(build_utils.FLAG_TEST_MARKER):  # type: ignore
             # Run if slow test marker is set: test in multiple environments
             # Python 3.6
-            build_python.test_with_py_version(python_version="3.6.12")
+            build_python.test_with_py_version(
+                python_version="3.6.12", exit_on_error=True
+            )
 
             # Python 3.7
-            build_python.test_with_py_version(python_version="3.7.9")
+            build_python.test_with_py_version(
+                python_version="3.7.9", exit_on_error=True
+            )
 
             # Activated Python Environment (3.8)
-            build_python.install_build_env()
+            build_python.install_build_env(exit_on_error=True)
             # Run pytest in pipenv environment
             build_utils.run("pipenv run pytest", exit_on_error=True)
 
@@ -66,6 +69,7 @@ def main(args: dict) -> None:
         build_python.publish_pypi_distribution(
             pypi_token=args.get(build_python.FLAG_PYPI_TOKEN),
             pypi_repository=args.get(build_python.FLAG_PYPI_REPOSITORY),
+            exit_on_error=True,
         )
 
         # TODO: Publish coverage report: if private repo set CODECOV_TOKEN="token" or use -t
@@ -73,4 +77,4 @@ def main(args: dict) -> None:
 
 
 if __name__ == "__main__":
-    main(build_python.get_sanitized_arguments())
+    main(build_python.parse_arguments())
